deafd829c1202eb8f05665f01e68cc4d13351f25
DavidKnight46 <nnCE6mpp!>
2022-09-03 16:07:06 +0100

    Edit fragment further implementation

--- app/src/main/java/com/example/libraryserviceandroidv2/EditActivity.kt
+++ app/src/main/java/com/example/libraryserviceandroidv2/EditActivity.kt
@@ -2,18 +2,23 @@ package com.example.libraryserviceandroidv2
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentContainer
+import androidx.fragment.app.FragmentContainerView
 import com.example.libraryserviceandroidv2.libraryservice.adapters.EditGamesAdapter
+import com.example.libraryserviceandroidv2.libraryservice.fragments.First2Fragment
 import com.example.libraryserviceandroidv2.libraryservice.gameobjects.GameList
 import com.example.libraryserviceandroidv2.libraryservice.gameobjects.IsAdded
+import com.google.android.material.textfield.TextInputEditText
 
 class EditActivity : AppCompatActivity() {
 
     @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         IsAdded.setIsAdded(false)
 
         setContentView(R.layout.layouteditactivity)
@@ -21,7 +26,10 @@ class EditActivity : AppCompatActivity() {
         var findViewById = findViewById<Spinner>(R.id.gamesToEdit)
 
         findViewById.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, GameList.getGameList())
-        findViewById.onItemSelectedListener = EditGamesAdapter(GameList.getGameList())
+        findViewById.onItemSelectedListener = EditGamesAdapter(GameList.getGameList(),
+            findViewById(R.id.fragmentViewContainer))
 
     }
+
+
 }
--- app/src/main/java/com/example/libraryserviceandroidv2/libraryservice/adapters/EditGamesAdapter.kt
+++ app/src/main/java/com/example/libraryserviceandroidv2/libraryservice/adapters/EditGamesAdapter.kt
@@ -2,14 +2,31 @@ package com.example.libraryserviceandroidv2.libraryservice.adapters
 
 import android.view.View
 import android.widget.AdapterView
+import android.widget.RatingBar
+import android.widget.Switch
+import androidx.fragment.app.FragmentContainerView
+import com.example.libraryserviceandroidv2.R
 import com.example.libraryserviceandroidv2.libraryservice.model.games.GameModel
+import com.google.android.material.textfield.TextInputEditText
 
-class EditGamesAdapter(val list: List<GameModel>) :  AdapterView.OnItemSelectedListener{
+class EditGamesAdapter(val list: List<GameModel>,
+                       var fragment: FragmentContainerView) :  AdapterView.OnItemSelectedListener{
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        println("smurf")
+        var gameModel = list.get(position)
+
+        setTextInput(fragment.findViewById(R.id.gameNameInput1), gameModel.name)
+        setTextInput(fragment.findViewById(R.id.releaseDateInput), gameModel.releaseDate)
+        setTextInput(fragment.findViewById(R.id.imageUrlInput), gameModel.imageUrl)
+
+        fragment.findViewById<RatingBar>(R.id.ratingBar).rating = gameModel.gameRating
+        fragment.findViewById<Switch>(R.id.preOrderSwitchAdd).isChecked = gameModel.preOrdered
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
         println("smurf")
+    }
+
+    private fun setTextInput(inputEdit : TextInputEditText, text: String){
+        inputEdit.setText(text)
     }
 }
